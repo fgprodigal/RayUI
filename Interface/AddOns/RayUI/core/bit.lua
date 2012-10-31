41af27fbfe082530187043f3529afec51dba8f11
fgprodigal <fgprodigal@gmail.com>
2012-10-31 20:16:34 +0800

    bit function fix

---
+++ Interface/AddOns/RayUI/core/bit.lua
@@ -0,0 +1,12 @@
+﻿local R, L, P = unpack(select(2, ...)) --Inport: Engine, Locales, ProfileDB
+
+if bit.bnot(0) ~= 0xFFFFFFFF then
+    local _bnot = bit.bnot
+    function bit.bnot(value)
+        if value < 0 then
+            return _bnot(value)
+        else
+            return 0xFFFFFFFF - value
+        end
+    end
+end
