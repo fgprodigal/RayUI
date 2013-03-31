9c0f110a043c4822dde260eb43f5d6c5a790516f
fgprodigal <fgprodigal@gmail.com>
2013-03-31 20:27:30 +0800

    questchoice frame

---
+++ Interface/AddOns/RayUI/modules/skins/blizzard/questchoice.lua
@@ -0,0 +1,37 @@
+local R, L, P = unpack(select(2, ...)) --Import: Engine, Locales, ProfileDB, local
+local S = R:GetModule("Skins")
+
+local function LoadSkin()
+    for i = 1, 18 do
+        select(i, QuestChoiceFrame:GetRegions()):Hide()
+    end
+
+    for i = 1, 2 do
+        local option = QuestChoiceFrame["Option"..i]
+        local rewards = option.Rewards
+        local icon = rewards.Item.Icon
+        local currencies = rewards.Currencies
+
+        option.OptionText:SetTextColor(.9, .9, .9)
+        rewards.Item.Name:SetTextColor(1, 1, 1)
+
+        icon:SetTexCoord(.08, .92, .08, .92)
+        icon:SetDrawLayer("BACKGROUND", 1)
+        S:CreateBG(icon)
+
+        for j = 1, 3 do
+            local cu = currencies["Currency"..j]
+
+            cu.Icon:SetTexCoord(.08, .92, .08, .92)
+            S:CreateBG(cu.Icon)
+        end
+    end
+
+    S:CreateBD(QuestChoiceFrame)
+    S:CreateSD(QuestChoiceFrame)
+    S:Reskin(QuestChoiceFrame.Option1.OptionButton)
+    S:Reskin(QuestChoiceFrame.Option2.OptionButton)
+    S:ReskinClose(QuestChoiceFrame.CloseButton)
+end
+
+S:RegisterSkin("Blizzard_QuestChoice", LoadSkin)
