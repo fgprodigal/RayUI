f32e4bd38b565040ad06f6e521980e5f69899c49
Ray <fgprodigal@gmail.com>
2013-10-08 21:52:19 +0800

    focuser修改, 尽力回避taint

--- Interface/AddOns/RayUI/modules/unitframes/focuser.lua
+++ Interface/AddOns/RayUI/modules/unitframes/focuser.lua
@@ -1,9 +1,55 @@
 local modifier = "shift" -- shift, alt or ctrl
 local mouseButton = "1" -- 1 = left, 2 = right, 3 = middle, 4 and 5 = thumb buttons if there are any
+local waitforHook = {}
+local FocuserButton
 
-function SetFocusHotkey(frame)
-	frame:SetAttribute(modifier.."-type"..mouseButton,"focus")
+local function SetFocusHotkey(frame)
+	if InCombatLockdown() then
+		waitforHook[frame] = true
+	else
+		frame:SetAttribute(modifier.."-type"..mouseButton,"focus")
+		waitforHook[frame] = nil
+	end
 end
+
+local eventFrame = CreateFrame("Frame", nil)
+eventFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
+eventFrame:RegisterEvent("PLAYER_LOGIN")
+eventFrame:SetScript("OnEvent", function(self, event)
+	if not FocuserButton then
+		-- Keybinding override so that models can be shift/alt/ctrl+clicked
+		FocuserButton = CreateFrame("CheckButton", "FocuserButton", UIParent, "SecureActionButtonTemplate")
+		FocuserButton:SetAttribute("type1","macro")
+		FocuserButton:SetAttribute("macrotext","/focus mouseover")
+		SetOverrideBindingClick(FocuserButton, true, modifier.."-BUTTON"..mouseButton, "FocuserButton")
+	end
+	if event=="PLAYER_LOGIN" then
+		-- Set the keybindings on the default unit frames since we won't get any CreateFrame notification about them
+		local duf = {
+			PlayerFrame,
+			PetFrame,
+			PartyMemberFrame1,
+			PartyMemberFrame2,
+			PartyMemberFrame3,
+			PartyMemberFrame4,
+			PartyMemberFrame1PetFrame,
+			PartyMemberFrame2PetFrame,
+			PartyMemberFrame3PetFrame,
+			PartyMemberFrame4PetFrame,
+			TargetFrame,
+		}
+
+		for i,frame in pairs(duf) do
+			SetFocusHotkey(frame)
+		end
+	else
+		if not InCombatLockdown() then
+			for frame in pairs(waitforHook) do
+				SetFocusHotkey(frame)
+			end
+		end
+	end
+end)
 
 local function CreateFrame_Hook(type, name, parent, template)
 	if name and name.lower and (name:lower():find("rayuf") or name:lower():find("rayuiraid")) then
@@ -12,28 +58,3 @@ local function CreateFrame_Hook(type, name, parent, template)
 end
 
 hooksecurefunc("CreateFrame", CreateFrame_Hook)
-
--- Keybinding override so that models can be shift/alt/ctrl+clicked
-local f = CreateFrame("CheckButton", "FocuserButton", UIParent, "SecureActionButtonTemplate")
-f:SetAttribute("type1","macro")
-f:SetAttribute("macrotext","/focus mouseover")
-SetOverrideBindingClick(FocuserButton,true,modifier.."-BUTTON"..mouseButton,"FocuserButton")
-
--- Set the keybindings on the default unit frames since we won't get any CreateFrame notification about them
-local duf = {
-	PlayerFrame,
-	PetFrame,
-	PartyMemberFrame1,
-	PartyMemberFrame2,
-	PartyMemberFrame3,
-	PartyMemberFrame4,
-	PartyMemberFrame1PetFrame,
-	PartyMemberFrame2PetFrame,
-	PartyMemberFrame3PetFrame,
-	PartyMemberFrame4PetFrame,
-	TargetFrame,
-}
-
-for i,frame in pairs(duf) do
-	SetFocusHotkey(frame)
-end
