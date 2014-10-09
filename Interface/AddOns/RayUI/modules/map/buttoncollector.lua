5857a1febe83ccd58d3dd1aea02580565f13d4b8
Ray <fgprodigal@gmail.com>
2014-10-09 19:14:10 +0800

    小地图按钮收集器更新

--- Interface/AddOns/RayUI/modules/map/buttoncollector.lua
+++ Interface/AddOns/RayUI/modules/map/buttoncollector.lua
@@ -6,7 +6,7 @@ local buttons = {}
 function MM:PositionButtonCollector(self, screenQuadrant)
 	local line = math.ceil(Minimap:GetWidth() / 20)
 	-- MBCF.bg:SetTexture(0, 0, 0, 1)
-	MBCF:SetAlpha(0)
+	-- MBCF:SetAlpha(0)
 	MBCF:ClearAllPoints()
 	screenQuadrant = screenQuadrant or R:GetScreenQuadrant(self)
 	for i =1, #buttons do
@@ -15,12 +15,12 @@ function MM:PositionButtonCollector(self, screenQuadrant)
 			buttons[i]:SetPoint("TOP", MBCF, "TOP", 0, 0)
 		elseif i%line == 1 then
 			if strfind(screenQuadrant, "RIGHT") then
-				buttons[i]:SetPoint("TOPRIGHT", buttons[i-line], "TOPLEFT", -1, 0)
+				buttons[i]:SetPoint("TOPRIGHT", buttons[i-line], "TOPLEFT", -5, 0)
 			else
-				buttons[i]:SetPoint("TOPLEFT", buttons[i-line], "TOPRIGHT", 1, 0)
+				buttons[i]:SetPoint("TOPLEFT", buttons[i-line], "TOPRIGHT", 5, 0)
 			end
 		else
-			buttons[i]:SetPoint("TOP", buttons[i-1], "BOTTOM", 0, -1)
+			buttons[i]:SetPoint("TOP", buttons[i-1], "BOTTOM", 0, -5)
 		end
 		buttons[i].ClearAllPoints = R.dummy
 		buttons[i].SetPoint = R.dummy
@@ -63,55 +63,99 @@ function MM:ButtonCollector()
 	-- MBCF.bg:SetAllPoints(MBCF)
 	-- MBCF.bg:SetGradientAlpha("VERTICAL", 0, 0, 0, 0, 0, 0, 0, .6)
 
+	function SetMinimapButton(btn)
+		if btn == nil or btn:GetName() == nil or btn:GetObjectType() ~= "Button" or btn:GetNumRegions() < 3 then return end
+		local name = btn:GetName()
+		if BlackList[name] then return end
+		btn:SetParent("MinimapButtonCollectFrame")
+		btn:SetPushedTexture(nil)
+		btn:SetHighlightTexture(nil)
+		btn:SetDisabledTexture(nil)
+		if not btn.isStyled then
+			btn.preset = {}
+			btn.preset.Width, btn.preset.Height = btn:GetSize()
+			btn.preset.Point, btn.preset.relativeTo, btn.preset.relativePoint, btn.preset.xOfs, btn.preset.yOfs = btn:GetPoint()
+			btn.preset.Parent = btn:GetParent()
+			btn.preset.FrameStrata = btn:GetFrameStrata()
+			btn.preset.FrameLevel = btn:GetFrameLevel()
+			btn.preset.Scale = btn:GetScale()
+			if btn:HasScript("OnDragStart") then 
+				btn.preset.DragStart = btn:GetScript("OnDragStart")
+			end 
+			if btn:HasScript("OnDragEnd") then 
+				btn.preset.DragEnd = btn:GetScript("OnDragEnd")
+			end
+			for i = 1, btn:GetNumRegions() do 
+				local frame = select(i, btn:GetRegions())
+				if frame:GetObjectType() == "Texture" then 
+					local iconFile = frame:GetTexture()
+					if(iconFile ~= nil and (iconFile:find("Border") or iconFile:find("Background") or iconFile:find("AlphaMask"))) then 
+						frame:SetTexture(0,0,0,0)
+					else
+						frame:ClearAllPoints()
+						frame:SetAllPoints()
+						frame:SetTexCoord(0.1, 0.9, 0.1, 0.9 )
+						frame:SetDrawLayer("ARTWORK")
+					end 
+				end 
+			end
 
+			btn:Size(20,20)
+			btn:CreateShadow("Background")
+
+			if(name == "DBMMinimapButton") then
+				btn.Hide = R.dummy
+				btn:Show()
+				btn:SetNormalTexture("Interface\\Icons\\INV_Helmet_87")
+			end 
+
+			if(name == "SmartBuff_MiniMapButton") then 
+				btn:SetNormalTexture(select(3, GetSpellInfo(12051)))
+			end
+
+			btn.isStyled = true
+
+			tinsert(buttons, btn)
+		end
+	end
+	
 	local MinimapButtonCollect = CreateFrame("Frame")
 	MinimapButtonCollect:RegisterEvent("PLAYER_ENTERING_WORLD")
 	MinimapButtonCollect:SetScript("OnEvent", function(self)
 		for i, child in ipairs({Minimap:GetChildren()}) do
-			if child:GetName() and not BlackList[child:GetName()] then
-				if child:GetObjectType() == "Button" and child:GetNumRegions() >= 3 then
-					child:SetParent("MinimapButtonCollectFrame")
-					for j = 1, child:GetNumRegions() do
-						local region = select(j, child:GetRegions())
-						if region:GetObjectType() == "Texture" then
-							local texture = region:GetTexture()
-							if texture == "Interface\\Minimap\\MiniMap-TrackingBorder" or texture == "Interface\\Minimap\\UI-Minimap-Background" or texture == "Interface\\Minimap\\UI-Minimap-ZoomButton-Highlight" then
-								region:Kill()
-							end
-						end
-					end
-					tinsert(buttons, child)
-				end
-			end
+			SetMinimapButton(child)
 		end
+
+		-- SetMinimapButton(BagSync_MinimapButton)
+
 		if #buttons == 0 then 
 			MBCF:Hide() 
 		else
-			for _, child in ipairs(buttons) do
-				child:HookScript("OnEnter", function()
-					UIFrameFadeIn(MBCF, .5, MBCF:GetAlpha(), 1)
-				end)
-				child:HookScript("OnLeave", function()
-					UIFrameFadeOut(MBCF, .5, MBCF:GetAlpha(), 0)
-				end)
-			end
+			-- for _, child in ipairs(buttons) do
+				-- child:HookScript("OnEnter", function()
+					-- UIFrameFadeIn(MBCF, .5, MBCF:GetAlpha(), 1)
+				-- end)
+				-- child:HookScript("OnLeave", function()
+					-- UIFrameFadeOut(MBCF, .5, MBCF:GetAlpha(), 0)
+				-- end)
+			-- end
 		end
 		MM:PositionButtonCollector(Minimap)
 	end)
 
-	MBCF:SetScript("OnEnter", function(self)
-		UIFrameFadeIn(self, .5, self:GetAlpha(), 1)
-	end)
+	-- MBCF:SetScript("OnEnter", function(self)
+		-- UIFrameFadeIn(self, .5, self:GetAlpha(), 1)
+	-- end)
 
-	Minimap:HookScript("OnEnter", function()
-		UIFrameFadeIn(MBCF, .5, MBCF:GetAlpha(), 1)
-	end)
+	-- Minimap:HookScript("OnEnter", function()
+		-- UIFrameFadeIn(MBCF, .5, MBCF:GetAlpha(), 1)
+	-- end)
 
-	MBCF:SetScript("OnLeave", function(self)
-		UIFrameFadeOut(self, .5, self:GetAlpha(), 0)
-	end)
+	-- MBCF:SetScript("OnLeave", function(self)
+		-- UIFrameFadeOut(self, .5, self:GetAlpha(), 0)
+	-- end)
 
-	Minimap:HookScript("OnLeave", function()
-		UIFrameFadeOut(MBCF, .5, MBCF:GetAlpha(), 0)
-	end)
+	-- Minimap:HookScript("OnLeave", function()
+		-- UIFrameFadeOut(MBCF, .5, MBCF:GetAlpha(), 0)
+	-- end)
 end
