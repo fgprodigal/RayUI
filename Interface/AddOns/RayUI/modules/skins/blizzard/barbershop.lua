cc14fdb1d3aee10e7efd850968ee12a28e3538e1
Ray <fgprodigal@gmail.com>
2014-10-25 19:16:02 +0800

    修复理发店报错

--- Interface/AddOns/RayUI/modules/skins/blizzard/barbershop.lua
+++ Interface/AddOns/RayUI/modules/skins/blizzard/barbershop.lua
@@ -2,20 +2,31 @@ local R, L, P = unpack(select(2, ...)) --Import: Engine, Locales, ProfileDB, loc
 local S = R:GetModule("Skins")
 
 local function LoadSkin()
-	S:SetBD(BarberShopFrame, 44, -75, -40, 44)
-	BarberShopFrameBackground:Hide()
+	BarberShopFrame:GetRegions():Hide()
 	BarberShopFrameMoneyFrame:GetRegions():Hide()
+	BarberShopAltFormFrameBackground:Hide()
+	BarberShopAltFormFrameBorder:Hide()
+
+	BarberShopAltFormFrame:ClearAllPoints()
+	BarberShopAltFormFrame:SetPoint("BOTTOM", BarberShopFrame, "TOP", 0, -74)
+
+	S:SetBD(BarberShopFrame, 44, -75, -40, 44)
+	S:SetBD(BarberShopAltFormFrame, 0, 0, 2, -2)
+
 	S:Reskin(BarberShopFrameOkayButton)
 	S:Reskin(BarberShopFrameCancelButton)
 	S:Reskin(BarberShopFrameResetButton)
-	S:ReskinArrow(BarberShopFrameSelector1Prev, "left")
-	S:ReskinArrow(BarberShopFrameSelector1Next, "right")
-	S:ReskinArrow(BarberShopFrameSelector2Prev, "left")
-	S:ReskinArrow(BarberShopFrameSelector2Next, "right")
-	S:ReskinArrow(BarberShopFrameSelector3Prev, "left")
-	S:ReskinArrow(BarberShopFrameSelector3Next, "right")
-	S:ReskinArrow(BarberShopFrameSelector4Prev, "left")
-	S:ReskinArrow(BarberShopFrameSelector4Next, "right")
+
+	for i = 1, 5 do
+		S:ReskinArrow(_G["BarberShopFrameSelector"..i.."Prev"], "left")
+		S:ReskinArrow(_G["BarberShopFrameSelector"..i.."Next"], "right")
+	end
+
+	-- [[ Banner frame ]]
+
+	BarberShopBannerFrameBGTexture:Hide()
+
+	S:SetBD(BarberShopBannerFrame, 25, -80, -20, 75)
 end
 
 S:RegisterSkin("Blizzard_BarbershopUI", LoadSkin)
