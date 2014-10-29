3aaee71b6b3ce4186b9db9b89ccc6717b782848c
Ray <fgprodigal@gmail.com>
2014-10-29 20:55:19 +0800

    自动贪婪逻辑修改

--- Interface/AddOns/RayUI/modules/misc/autodez.lua
+++ Interface/AddOns/RayUI/modules/misc/autodez.lua
@@ -6,16 +6,10 @@ local function LoadFunc()
 	autogreed:RegisterEvent("START_LOOT_ROLL")
 	autogreed:SetScript("OnEvent", function(self, event, id)
         if not M.db.autodez then return end
-		local name = select(2, GetLootRollItemInfo(id))
-
-		--Auto Need Chaos Orb
-		if (name == select(1, GetItemInfo(52078))) then
-			RollOnLoot(id, 1)
-		end
-
 		if UnitLevel("player") ~= MAX_PLAYER_LEVEL then return end
-		if(id and select(4, GetLootRollItemInfo(id))==2 and not (select(5, GetLootRollItemInfo(id)))) then
-			if RollOnLoot(id, 3) then
+		local texture, name, count, quality, bop, canNeed, canGreed, canDisenchant = GetLootRollItemInfo(id)
+		if id and quality == 2 and not bop then
+			if canDisenchant and IsSpellKnown(13262) then
 				RollOnLoot(id, 3)
 			else
 				RollOnLoot(id, 2)
