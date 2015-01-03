94cd9e24e0e659aa35d2770c6c819e4029db1d93
Ray <fgprodigal@gmail.com>
2015-01-03 19:29:17 +0800

    修复一处动作条设置

--- Interface/AddOns/RayUI/modules/actionbar/actionbar.lua
+++ Interface/AddOns/RayUI/modules/actionbar/actionbar.lua
@@ -412,7 +412,6 @@ function AB:UpdateButtonConfig(bar, buttonName)
 	bar.buttonConfig.hideElements.hotkey = self.db.hotkeys
 	bar.buttonConfig.showGrid = self.db.showgrid
 	bar.buttonConfig.clickOnDown = self.db.clickondown
-	SetModifiedClick("PICKUPACTION", self.db.movementModifier)
 	bar.buttonConfig.colors.range = { 1, 0.3, 0.1 }
 	bar.buttonConfig.colors.mana = { 0.1, 0.3, 1 }
 	bar.buttonConfig.colors.hp = { 0.1, 0.3, 1 }
