28fcb3d74c28aea3bfb6c2f5dcba371312797362
Kaguya <386949142@qq.com>
2015-08-07 14:20:12 +0800

    Fix altpower

--- Interface/AddOns/RayUI/modules/misc/altpower.lua
+++ Interface/AddOns/RayUI/modules/misc/altpower.lua
@@ -3,13 +3,17 @@ local M = R:GetModule("Misc")
 
 local function LoadFunc()
 	local holder = CreateFrame("Frame", "AltPowerBarHolder", UIParent)
-	holder:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 100)
+	holder:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 70)
 	holder:Size(128, 50)
 
 	PlayerPowerBarAlt:ClearAllPoints()
 	PlayerPowerBarAlt:SetPoint("CENTER", holder, "CENTER")
 	PlayerPowerBarAlt:SetParent(holder)
 	PlayerPowerBarAlt.ignoreFramePositionManager = true
+
+	hooksecurefunc(PlayerPowerBarAlt, "ClearAllPoints", function(self)
+		self:SetPoint('CENTER', AltPowerBarHolder, 'CENTER')
+	end)
 	
 	R:CreateMover(holder, "AltPowerBarMover", L["副资源条"])
 end
