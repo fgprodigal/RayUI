ec9fb19e6c255c7154decb5cb0729b9539b2e58c
Ray <fgprodigal@gmail.com>
2016-09-29 22:58:17 +0800

    update questchoice skin

--- Interface/AddOns/RayUI/modules/skins/blizzard/questchoice.lua
+++ Interface/AddOns/RayUI/modules/skins/blizzard/questchoice.lua
@@ -10,20 +10,19 @@ local function LoadSkin()
 		select(i, QuestChoiceFrame:GetRegions()):Hide()
 	end
 	
-	for i = 1, 4 do
+	for i = 1, #QuestChoiceFrame.Options do
 		local option = QuestChoiceFrame["Option"..i]
 		local rewards = option.Rewards
-		local icon = rewards.Item.Icon
+		local item = rewards.Item
 		local currencies = rewards.Currencies
 
 		option.OptionText:SetTextColor(.9, .9, .9)
-		rewards.Item.Name:SetTextColor(1, 1, 1)
 
-		icon:SetTexCoord(.08, .92, .08, .92)
-		icon:SetDrawLayer("BACKGROUND", 1)
-		S:CreateBG(icon)
-	
-		S:Reskin(option.OptionButton)
+		item.Name:SetTextColor(1, 1, 1)
+		item.Icon:SetTexCoord(.08, .92, .08, .92)
+		item.Icon:SetDrawLayer("BACKGROUND", 1)
+		item.bg = S:CreateBG(item.Icon)
+
 
 		for j = 1, 3 do
 			local cu = currencies["Currency"..j]
@@ -31,10 +30,18 @@ local function LoadSkin()
 			cu.Icon:SetTexCoord(.08, .92, .08, .92)
 			S:CreateBG(cu.Icon)
 		end
+		S:Reskin(option.OptionButton)
 	end
 
-	S:CreateBD(QuestChoiceFrame)
-	S:CreateSD(QuestChoiceFrame)
+	hooksecurefunc("QuestChoiceFrame_ShowRewards", function(numOptions)
+		for i = 1, numOptions do
+			local rewards = QuestChoiceFrame["Option"..i].Rewards
+			rewards.Item.Name:SetTextColor(rewards.Item.IconBorder:GetVertexColor())
+			rewards.Item.IconBorder:Hide()
+		end
+	end)
+
+	S:SetBD(QuestChoiceFrame)
 	S:ReskinClose(QuestChoiceFrame.CloseButton)
 end
 
