13c68e12f72f45ce35fc8ec758e3e149db9f0b5a
Ray <fgprodigal@gmail.com>
2016-10-05 10:44:10 +0800

    Cached global variables in files in the blizzard folder

--- Interface/AddOns/RayUI/modules/blizzards/blizzards.lua
+++ Interface/AddOns/RayUI/modules/blizzards/blizzards.lua
@@ -2,8 +2,6 @@ local R, L, P, G = unpack(select(2, ...)) --Import: Engine, Locales, ProfileDB, 
 local B = R:NewModule("Blizzards", "AceEvent-3.0", "AceHook-3.0", "AceConsole-3.0")
 
 function B:Initialize()
-	self:FixDeathPopup()
-	self:FixBlizzardBugs()
 end
 
 R:RegisterModule(B:GetName())
