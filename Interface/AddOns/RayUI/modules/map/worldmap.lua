498d060da9c60732eb3c4a465b798f355e84c676
Ray <fgprodigal@gmail.com>
2016-10-27 14:33:06 +0800

    fix #4.

--- Interface/AddOns/RayUI/modules/map/worldmap.lua
+++ Interface/AddOns/RayUI/modules/map/worldmap.lua
@@ -22,111 +22,128 @@ local GetCursorPosition = GetCursorPosition
 WM.modName = L["世界地图"]
 
 function WM:SetLargeWorldMap()
-	if InCombatLockdown() then return end
+    if InCombatLockdown() then return end
 
-	WorldMapFrame:SetParent(R.UIParent)
-	WorldMapFrame:EnableKeyboard(false)
-	WorldMapFrame:SetScale(1)
-	WorldMapFrame:EnableMouse(true)
+    WorldMapFrame:SetParent(R.UIParent)
+    WorldMapFrame:EnableKeyboard(false)
+    WorldMapFrame:SetScale(1)
+    WorldMapFrame:EnableMouse(true)
 
-	if WorldMapFrame:GetAttribute("UIPanelLayout-area") ~= "center" then
-		SetUIPanelAttribute(WorldMapFrame, "area", "center");
-	end
+    if WorldMapFrame:GetAttribute("UIPanelLayout-area") ~= "center" then
+        SetUIPanelAttribute(WorldMapFrame, "area", "center");
+    end
 
-	if WorldMapFrame:GetAttribute("UIPanelLayout-allowOtherPanels") ~= true then
-		SetUIPanelAttribute(WorldMapFrame, "allowOtherPanels", true)
-	end
+    if WorldMapFrame:GetAttribute("UIPanelLayout-allowOtherPanels") ~= true then
+        SetUIPanelAttribute(WorldMapFrame, "allowOtherPanels", true)
+    end
 
-	WorldMapFrameSizeUpButton:Hide()
-	WorldMapFrameSizeDownButton:Show()
+    WorldMapFrameSizeUpButton:Hide()
+    WorldMapFrameSizeDownButton:Show()
 
-	WorldMapFrame:ClearAllPoints()
-	WorldMapFrame:SetPoint("CENTER", R.UIParent, "CENTER", 0, 100)
-	WorldMapFrame:SetSize(1002, 668)
+    WorldMapFrame:ClearAllPoints()
+    WorldMapFrame:SetPoint("CENTER", R.UIParent, "CENTER", 0, 100)
+    WorldMapFrame:SetSize(1002, 668)
 end
 
 function WM:SetSmallWorldMap()
-	if InCombatLockdown() then return; end
+    if InCombatLockdown() then return; end
 
-	WorldMapFrameSizeUpButton:Show()
-	WorldMapFrameSizeDownButton:Hide()
+    WorldMapFrameSizeUpButton:Show()
+    WorldMapFrameSizeDownButton:Hide()
 end
 
 function WM:PLAYER_REGEN_ENABLED()
-	WorldMapFrameSizeDownButton:Enable()
-	WorldMapFrameSizeUpButton:Enable()
+    WorldMapFrameSizeDownButton:Enable()
+    WorldMapFrameSizeUpButton:Enable()
 end
 
 function WM:PLAYER_REGEN_DISABLED()
-	WorldMapFrameSizeDownButton:Disable()
-	WorldMapFrameSizeUpButton:Disable()
-end
-
-function WM:UpdateCoords()
-	if not WorldMapFrame:IsShown() then return end
-	local inInstance, _ = IsInInstance()
-	local x, y = GetPlayerMapPosition("player")
-	x = R:Round(100 * x, 2)
-	y = R:Round(100 * y, 2)
-
-	if x ~= 0 and y ~= 0 then
-		CoordsHolder.playerCoords:SetText(PLAYER..": "..x..", "..y)
-	else
-		CoordsHolder.playerCoords:SetText(nil)
-	end
-
-	local scale = WorldMapDetailFrame:GetEffectiveScale()
-	local width = WorldMapDetailFrame:GetWidth()
-	local height = WorldMapDetailFrame:GetHeight()
-	local centerX, centerY = WorldMapDetailFrame:GetCenter()
-	local x, y = GetCursorPosition()
-	local adjustedX = (x / scale - (centerX - (width/2))) / width
-	local adjustedY = (centerY + (height/2) - y / scale) / height
-
-	if (adjustedX >= 0  and adjustedY >= 0 and adjustedX <= 1 and adjustedY <= 1) then
-		adjustedX = R:Round(100 * adjustedX, 2)
-		adjustedY = R:Round(100 * adjustedY, 2)
-		CoordsHolder.mouseCoords:SetText(MOUSE_LABEL..": "..adjustedX..", "..adjustedY)
-	else
-		CoordsHolder.mouseCoords:SetText(nil)
-	end
+    WorldMapFrameSizeDownButton:Disable()
+    WorldMapFrameSizeUpButton:Disable()
 end
 
 function WM:ResetDropDownListPosition(frame)
-	DropDownList1:ClearAllPoints()
-	DropDownList1:Point("TOPRIGHT", frame, "BOTTOMRIGHT", -17, -4)
+    DropDownList1:ClearAllPoints()
+    DropDownList1:Point("TOPRIGHT", frame, "BOTTOMRIGHT", -17, -4)
+end
+
+local inRestrictedArea = false
+function WM:PLAYER_ENTERING_WORLD()
+    local x = GetPlayerMapPosition("player")
+    if not x then
+        inRestrictedArea = true
+        self:CancelTimer(self.CoordsTimer)
+        self.CoordsTimer = nil
+        CoordsHolder.playerCoords:SetText("")
+        CoordsHolder.mouseCoords:SetText("")
+    elseif not self.CoordsTimer then
+        inRestrictedArea = false
+        self.CoordsTimer = self:ScheduleRepeatingTimer("UpdateCoords", 0.05)
+    end
+end
+
+function WM:UpdateCoords()
+    if not WorldMapFrame:IsShown() then return end
+    local inInstance, _ = IsInInstance()
+    local x, y = GetPlayerMapPosition("player")
+
+    x = R:Round(100 * x, 2)
+    y = R:Round(100 * y, 2)
+
+    if x ~= 0 and y ~= 0 then
+        CoordsHolder.playerCoords:SetText(PLAYER..": "..x..", "..y)
+    else
+        CoordsHolder.playerCoords:SetText(nil)
+    end
+
+    local scale = WorldMapDetailFrame:GetEffectiveScale()
+    local width = WorldMapDetailFrame:GetWidth()
+    local height = WorldMapDetailFrame:GetHeight()
+    local centerX, centerY = WorldMapDetailFrame:GetCenter()
+    local x, y = GetCursorPosition()
+    local adjustedX = (x / scale - (centerX - (width/2))) / width
+    local adjustedY = (centerY + (height/2) - y / scale) / height
+
+    if (adjustedX >= 0 and adjustedY >= 0 and adjustedX <= 1 and adjustedY <= 1) then
+        adjustedX = R:Round(100 * adjustedX, 2)
+        adjustedY = R:Round(100 * adjustedY, 2)
+        CoordsHolder.mouseCoords:SetText(MOUSE_LABEL..": "..adjustedX..", "..adjustedY)
+    else
+        CoordsHolder.mouseCoords:SetText(nil)
+    end
 end
 
 function WM:Initialize()
-	local CoordsHolder = CreateFrame("Frame", "CoordsHolder", WorldMapFrame)
-	CoordsHolder:SetFrameLevel(WorldMapDetailFrame:GetFrameLevel() + 1)
-	CoordsHolder:SetFrameStrata(WorldMapDetailFrame:GetFrameStrata())
-	CoordsHolder.playerCoords = CoordsHolder:CreateFontString(nil, "OVERLAY")
-	CoordsHolder.mouseCoords = CoordsHolder:CreateFontString(nil, "OVERLAY")
-	CoordsHolder.playerCoords:SetFontObject(NumberFontNormal)
-	CoordsHolder.mouseCoords:SetFontObject(NumberFontNormal)
-	CoordsHolder.playerCoords:SetText(PLAYER..":   0, 0")
-	CoordsHolder.mouseCoords:SetText(MOUSE_LABEL..":   0, 0")
-	CoordsHolder.playerCoords:SetPoint("BOTTOMLEFT", WorldMapFrame.UIElementsFrame, "BOTTOMLEFT", 5, 5)
-	CoordsHolder.mouseCoords:SetPoint("BOTTOMLEFT", CoordsHolder.playerCoords, "TOPLEFT")
+    local CoordsHolder = CreateFrame("Frame", "CoordsHolder", WorldMapFrame)
+    CoordsHolder:SetFrameLevel(WorldMapDetailFrame:GetFrameLevel() + 1)
+    CoordsHolder:SetFrameStrata(WorldMapDetailFrame:GetFrameStrata())
+    CoordsHolder.playerCoords = CoordsHolder:CreateFontString(nil, "OVERLAY")
+    CoordsHolder.mouseCoords = CoordsHolder:CreateFontString(nil, "OVERLAY")
+    CoordsHolder.playerCoords:SetFontObject(NumberFontNormal)
+    CoordsHolder.mouseCoords:SetFontObject(NumberFontNormal)
+    CoordsHolder.playerCoords:SetText(PLAYER..": 0, 0")
+    CoordsHolder.mouseCoords:SetText(MOUSE_LABEL..": 0, 0")
+    CoordsHolder.playerCoords:SetPoint("BOTTOMLEFT", WorldMapFrame.UIElementsFrame, "BOTTOMLEFT", 5, 5)
+    CoordsHolder.mouseCoords:SetPoint("BOTTOMLEFT", CoordsHolder.playerCoords, "TOPLEFT")
 
-	self:ScheduleRepeatingTimer("UpdateCoords", 0.05)
+    self.CoordsTimer = self:ScheduleRepeatingTimer("UpdateCoords", 0.05)
+    self:RegisterEvent("PLAYER_ENTERING_WORLD")
 
-	BlackoutWorld:SetTexture(nil)
-	self:SecureHook("WorldMap_ToggleSizeDown", "SetSmallWorldMap")
-	self:SecureHook("WorldMap_ToggleSizeUp", "SetLargeWorldMap")
-	self:RegisterEvent("PLAYER_REGEN_ENABLED")
-	self:RegisterEvent("PLAYER_REGEN_DISABLED")
+    BlackoutWorld:SetTexture(nil)
+    self:SecureHook("WorldMap_ToggleSizeDown", "SetSmallWorldMap")
+    self:SecureHook("WorldMap_ToggleSizeUp", "SetLargeWorldMap")
+    self:RegisterEvent("PLAYER_REGEN_ENABLED")
+    self:RegisterEvent("PLAYER_REGEN_DISABLED")
 
-	if WORLDMAP_SETTINGS.size == WORLDMAP_FULLMAP_SIZE then
-		self:SetLargeWorldMap()
-	elseif WORLDMAP_SETTINGS.size == WORLDMAP_WINDOWED_SIZE then
-		self:SetSmallWorldMap()
-	end
+    if WORLDMAP_SETTINGS.size == WORLDMAP_FULLMAP_SIZE then
+        self:SetLargeWorldMap()
+    elseif WORLDMAP_SETTINGS.size == WORLDMAP_WINDOWED_SIZE then
+        self:SetSmallWorldMap()
+    end
 end
 
 function WM:Info()
-	return L["|cff7aa6d6Ray|r|cffff0000U|r|cff7aa6d6I|r世界地图模块."]
+    return L["|cff7aa6d6Ray|r|cffff0000U|r|cff7aa6d6I|r世界地图模块."]
 end
 
 R:RegisterModule(WM:GetName())
