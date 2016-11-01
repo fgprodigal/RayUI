32bb7597c142f3fa4442d5af27677d5538cd837e
Ray <fgprodigal@gmail.com>
2016-11-01 19:57:53 +0800

    update for new lib name

--- Interface/AddOns/RayUI/modules/actionbar/actionbar.lua
+++ Interface/AddOns/RayUI/modules/actionbar/actionbar.lua
@@ -1,7 +1,7 @@
 ﻿--ActionBar from ElvUI
 local R, L, P, G = unpack(select(2, ...)) --Import: Engine, Locales, ProfileDB, GlobalDB
 local AB = R:NewModule("ActionBar", "AceEvent-3.0", "AceHook-3.0", "AceConsole-3.0")
-local LAB = LibStub("LibActionButton-1.0")
+local LAB = LibStub("LibActionButton-1.0-RayUI")
 
 --Cache global variables
 --Lua functions
