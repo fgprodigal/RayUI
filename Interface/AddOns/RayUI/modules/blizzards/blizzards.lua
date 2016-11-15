76520e75bfdf85344f629cdadaf303e931b8f882
fgprodigal <fgprodigal@gmail.com>
2016-11-15 16:34:06 +0800

    try to fix joinbattlefield taint.

--- Interface/AddOns/RayUI/modules/blizzards/blizzards.lua
+++ Interface/AddOns/RayUI/modules/blizzards/blizzards.lua
@@ -2,6 +2,11 @@ local R, L, P, G = unpack(select(2, ...)) --Import: Engine, Locales, ProfileDB, 
 local B = R:NewModule("Blizzards", "AceEvent-3.0", "AceHook-3.0", "AceConsole-3.0")
 
 function B:Initialize()
+    CreateFrame("Frame"):SetScript("OnUpdate", function(self, elapsed)
+		if LFRBrowseFrame.timeToClear then
+			LFRBrowseFrame.timeToClear = nil
+		end
+	end)
 end
 
 R:RegisterModule(B:GetName())
