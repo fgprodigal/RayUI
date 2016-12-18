39d2a18e64d599983c0da1a317912aeea59ad318
Ray <fgprodigal@gmail.com>
2016-12-18 22:21:58 +0800

    remove unused file

--- Interface/AddOns/RayUI/modules/unitframes/filter.lua
+++
@@ -1,9 +0,0 @@
-local R, L, P, G = unpack(select(2, ...)) --Import: Engine, Locales, ProfileDB, GlobalDB
-local UF = R:GetModule("UnitFrames")
-
-UF.PvEMeleeBossDebuffs = {
-}
-UF.PvECasterBossDebuffs = {
-}
-UF.PvETankBossDebuffs = {
-}
