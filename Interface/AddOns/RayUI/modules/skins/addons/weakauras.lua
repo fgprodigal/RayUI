29907768ce8797646408e2f150fd90ccdf5ed6f8
Ray <fgprodigal@gmail.com>
2016-12-22 19:50:30 +0800

    add skin for weakauras

---
+++ Interface/AddOns/RayUI/modules/skins/addons/weakauras.lua
@@ -0,0 +1,56 @@
+local R, L, P, G = unpack(select(2, ...)) --Import: Engine, Locales, ProfileDB, GlobalDB
+local S = R:GetModule("Skins")
+
+local function SkinWeakAuras()
+    local function Skin_WeakAuras(frame, ftype)
+        if ftype == "icon" then
+            if not frame.shadow then
+                frame:CreateShadow("Background")
+                frame.icon:SetTexCoord(.08, .92, .08, .92)
+                frame.icon.SetTexCoord = R.dummy
+            end
+        end
+
+        if ftype == "aurabar" then
+            if not frame.bar.shadow then
+                frame.bar:CreateShadow("Background")
+                frame.iconFrame:CreateShadow("Background")
+                frame.iconFrame:SetAllPoints(frame.icon)
+                frame.icon:SetTexCoord(.08, .92, .08, .92)
+                frame.icon.SetTexCoord = R.dummy
+            end
+        end
+    end
+    local Create_Icon, Modify_Icon = WeakAuras.regionTypes.icon.create, WeakAuras.regionTypes.icon.modify
+    local Create_AuraBar, Modify_AuraBar = WeakAuras.regionTypes.aurabar.create, WeakAuras.regionTypes.aurabar.modify
+    WeakAuras.regionTypes.icon.create = function(parent, data)
+        local region = Create_Icon(parent, data)
+        Skin_WeakAuras(region, "icon")
+        return region
+    end
+
+    WeakAuras.regionTypes.aurabar.create = function(parent)
+        local region = Create_AuraBar(parent)
+        Skin_WeakAuras(region, "aurabar")
+        return region
+    end
+
+    WeakAuras.regionTypes.icon.modify = function(parent, region, data)
+        Modify_Icon(parent, region, data)
+        Skin_WeakAuras(region, "icon")
+    end
+
+    WeakAuras.regionTypes.aurabar.modify = function(parent, region, data)
+        Modify_AuraBar(parent, region, data)
+        Skin_WeakAuras(region, "aurabar")
+    end
+
+    for weakAura, _ in pairs(WeakAuras.regions) do
+        if WeakAuras.regions[weakAura].regionType == "icon"
+        or WeakAuras.regions[weakAura].regionType == "aurabar" then
+            Skin_WeakAuras(WeakAuras.regions[weakAura].region, WeakAuras.regions[weakAura].regionType)
+        end
+    end
+end
+
+S:AddCallbackForAddon("WeakAuras", "WeakAuras", SkinWeakAuras)
