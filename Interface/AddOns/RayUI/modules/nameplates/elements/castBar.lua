55686285ba9206c6c6078849ed1614c1eb40a694
Ray <fgprodigal@gmail.com>
2018-08-22 11:04:54 +0800

    fix for UnitCastingInfo

--- Interface/AddOns/RayUI/modules/nameplates/elements/castBar.lua
+++ Interface/AddOns/RayUI/modules/nameplates/elements/castBar.lua
@@ -56,7 +56,7 @@ function mod:UpdateElement_Cast(frame, event, ...)
     end
 
     if ( event == "UNIT_SPELLCAST_START" ) then
-        local name, _, text, texture, startTime, endTime, _, castID, notInterruptible = UnitCastingInfo(unit);
+        local name, text, texture, startTime, endTime, _, castID, notInterruptible = UnitCastingInfo(unit);
         if ( not name) then
             frame.CastBar:Hide();
             return;
@@ -123,7 +123,7 @@ function mod:UpdateElement_Cast(frame, event, ...)
         end
     elseif ( event == "UNIT_SPELLCAST_DELAYED" ) then
         if ( frame:IsShown() ) then
-            local name, _, _, _, startTime, endTime, _, _, notInterruptible = UnitCastingInfo(unit);
+            local name, _, _, startTime, endTime, _, _, notInterruptible = UnitCastingInfo(unit);
             if ( not name ) then
                 -- if there is no name, there is no bar
                 frame.CastBar:Hide();
