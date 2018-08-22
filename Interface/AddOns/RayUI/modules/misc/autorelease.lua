4f0fdb1440684a4fd20936d1afdc1d6c351d1083
Ray <fgprodigal@gmail.com>
2018-08-22 11:05:19 +0800

    replace UnitBuff with AuraUtil.FindAuraByName

--- Interface/AddOns/RayUI/modules/misc/autorelease.lua
+++ Interface/AddOns/RayUI/modules/misc/autorelease.lua
@@ -10,7 +10,7 @@ local mod = M:NewModule("AutoRelease", "AceEvent-3.0")
 function mod:PLAYER_DEAD()
     if not M.db.autorelease then return end
     local soulstone = GetSpellInfo(20707)
-    if ((R.myclass ~= "SHAMAN") and not (soulstone and UnitBuff("player", soulstone))) then
+    if ((R.myclass ~= "SHAMAN") and not (soulstone and AuraUtil.FindAuraByName(soulstone, "player", "HELPFUL"))) then
         for i=1,5 do
             local status = GetBattlefieldStatus(i)
             if status == "active" then
